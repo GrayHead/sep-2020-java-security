068789b84c76332fd354d3212a5430d4fdd3a02b
GrayHead <ss.zhuravlov@gmail.com>
2021-04-01 20:27:56 +0300

    delete

--- src/main/java/com/example/security/SecurityApplication.java
+++ src/main/java/com/example/security/SecurityApplication.java
@@ -2,12 +2,20 @@ package com.example.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class SecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
     }
+
+//    @GetMapping("/**") // /users
+//    public String home() {
+//        return "forward:/index.html";
+//    }
 
 }
